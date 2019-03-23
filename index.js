fdd7f9efb0ad38175a47e1757346050015ae6a56
Ronald Langeveld <ronald@Ronalds-MacBook-Pro.local>
2019-03-23 16:57:25 +0900

    added minus fix

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ const app = express();
 app.use(express.json());
 app.options('*', cors()) 
 
+    // Gets DATA from http://loadshedding.eskom.co.za/LoadShedding/GetStatus
+    // This number is 1 digit ahead of actual status, so subtract 1 to get actual status.
+
 app.get('/eskomStatus', cors(), (request, response) => {
 
     axios.get('http://loadshedding.eskom.co.za/LoadShedding/GetStatus').then(res => {
-        const status = res.data
+        const stval = res.data;
+        const val = parseInt(stval)
+        var status = val - 1;
         response.json({stage: status})
     })
 
- 
-    // http://loadshedding.eskom.co.za/LoadShedding/GetStatus
+
 
   });
 
